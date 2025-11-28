c1a8c2f78be6baea8041113b920bd957379cfaf5
RiNa-036 <it24036@mbstu.ac.bd>
2025-11-28 23:31:20 +0600

    Create Ex8.java

---
+++ Liang BOOK/Chapter-1/Ex8.java
@@ -0,0 +1,9 @@
+public class Ex8 {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double radius = 5.6;
+        double perimeter = 2*radius*pi;
+        double area = radius*radius*pi;
+        System.out.println("Area of the circle is: "+ area + "and perimeter of the circle is:" + perimeter);
+    }  
+}
