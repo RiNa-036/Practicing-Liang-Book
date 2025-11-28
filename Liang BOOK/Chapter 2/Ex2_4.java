eae73c4217706ca7e60d811a32a2b4abab9edb7c
RiNa-036 <it24036@mbstu.ac.bd>
2025-11-28 23:39:04 +0600

    Create Ex2_4.java

---
+++ Liang BOOK/Chapter 2/Ex2_4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex2_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter a feet value: ");
+        double feet = input.nextDouble();
+
+        double meter = feet/0.305;
+
+        System.out.println(feet + " feet is " + meter + "meters");
+      
+        input.close();
+
+    }
+    
+}
